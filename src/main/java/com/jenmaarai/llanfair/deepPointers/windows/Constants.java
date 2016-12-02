6dba11a51ce02e6d6995487dfb0b082794658279
Guillaume Turchini <guillaume.turchini@gmail.com>
2016-12-02 23:40:19 +0100

    Windows constants

---
+++ src/main/java/com/jenmaarai/llanfair/deepPointers/windows/Constants.java
@@ -0,0 +1,8 @@
+package com.jenmaarai.llanfair.deepPointers.windows;
+
+class Constants {
+   public static final int PROCESS_VM_READ= 0x0010;
+   public static final int PROCESS_VM_WRITE = 0x0020;
+   public static final int PROCESS_VM_OPERATION = 0x0008;
+   public static final int PROCESS_QUERY_INFORMATION = 0x0400;
+}
