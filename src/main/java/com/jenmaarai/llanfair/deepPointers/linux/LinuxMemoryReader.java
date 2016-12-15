75002658e920aa75aac810ba9dc20f10f6297272
Guillaume Turchini <guillaume.turchini@gmail.com>
2016-12-15 01:12:13 +0100

    First version of MemoryReader for linux.
    This is missing window title reading...
    
    As a security feature, the kernel forbids to attach to process that are
    not your direct child. You may need to change
    /proc/sys/kernel/yama/ptrace_scope to 0 to let llanfair read the memory,
    or run llanfair as root.

---
+++ src/main/java/com/jenmaarai/llanfair/deepPointers/linux/LinuxMemoryReader.java
@@ -0,0 +1,123 @@
+package com.jenmaarai.llanfair.deepPointers.linux;
+
+import com.jenmaarai.llanfair.deepPointers.MemoryReader;
+import com.jenmaarai.llanfair.deepPointers.MemoryReaderSingleton;
+import com.jenmaarai.llanfair.deepPointers.Process;
+import com.jenmaarai.llanfair.deepPointers.exceptions.MemoryReaderException;
+import com.jenmaarai.llanfair.deepPointers.linux.nativeInterface.LibC;
+import com.jenmaarai.llanfair.deepPointers.linux.nativeInterface.iovec;
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LinuxMemoryReader implements MemoryReader {
+   private static final Logger LOG = LoggerFactory.getLogger(LinuxMemoryReader.class);
+   private static final int BUFFER_SIZE = 1024;
+   private LibC libC = (LibC) Native.loadLibrary("c", LibC.class);
+   
+   @Override
+   public List<Process> getAllProcesses(boolean onlyReadable) throws MemoryReaderException {
+      List<Process> l = new ArrayList<>();
+      
+      File parent = new File("/proc");
+      File[] childs = parent.listFiles();
+      if (childs == null) {
+         throw new MemoryReaderException("Can't list directory");
+      }
+      
+      for (File f : childs) {
+         if (f.getName().matches("[0-9]+")) {
+            int pid = Integer.parseInt(f.getName());
+            
+            File cmdFile = new File("/proc/" + pid + "/cmdline");
+            String cmdline;
+            
+            try (InputStream is = new FileInputStream(cmdFile)) {
+               byte[] buf = new byte[BUFFER_SIZE];
+               int readBytes = is.read(buf);
+               if (readBytes <= 0) {
+                  throw new IOException("Nothing to read from this reader");
+               }
+               cmdline = new String(buf, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+               LOG.debug("Exception got during reading of command line args of process {} : {}", pid, e);
+               continue;
+            }
+            
+            String filename = cmdline.isEmpty() ? "" : cmdline.split("\u0000")[0];
+            
+            // This is a process
+            File memFile = new File("/proc/" + pid + "/mem");
+            
+            if (memFile.canRead()) {
+               // Get name
+               
+               l.add(new LinuxProcess(pid, filename, "", true));
+            } else {
+               if (!onlyReadable) {
+                  l.add(new LinuxProcess(pid, filename, "", false));
+               }
+            }
+         }
+      }
+      
+      return l;
+   }
+   
+   @Override
+   public List<Process> getAllVisibleProcesses() throws MemoryReaderException {
+      return Collections.emptyList();
+   }
+   
+   @Override
+   public Process getProcessFromPid(int pid) throws MemoryReaderException {
+      return new LinuxProcess(pid, "", "", false);
+   }
+   
+   @Override
+   public byte[] readMemory(Process p, long address, int size) throws MemoryReaderException {
+      Memory nativeMem = new Memory(size);
+      
+      iovec local = new iovec(nativeMem, size);
+      iovec remote = new iovec(new Pointer(address), size);
+      
+      if(libC.process_vm_readv(p.getPid(), local, 1, remote, 1, 0) != size){
+         libC.perror("");
+         throw new MemoryReaderException("Can't read?");
+      }
+      
+      byte[] buf = new byte[size];
+      nativeMem.read(0, buf, 0, size);
+      
+      return buf;
+   }
+   
+   @Override
+   public long readInteger(Process p, long address, int size) throws MemoryReaderException {
+      byte[] mem = this.readMemory(p, address, size);
+      
+      if (!MemoryReaderSingleton.bigEndian) {
+         // Reverse the byte array
+         byte temp;
+         for (int i = 0; i < size / 2; i++) {
+            temp = mem[i];
+            mem[i] = mem[size - 1 - i];
+            mem[size - 1 - i] = temp;
+         }
+      }
+      
+      return new BigInteger(mem).longValue();
+   }
+}
